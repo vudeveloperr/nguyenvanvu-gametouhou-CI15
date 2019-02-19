0e2da881b56761890c8a60d5c600e30501e40b91
nguyenvanvu <vubaojr99@gmail.com>
2019-02-19 20:49:14 +0700

    session 4(đang làm dở)

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -9,21 +9,11 @@ import java.util.ArrayList;
 
 public class GamePanel extends JPanel {
     Background background;
-    BufferedImage playerImage;
-    Vector2D playerPosition;
-    BufferedImage bulletImage;
-//    Vector2D[] bulletPositions;
-    ArrayList<Vector2D> bulletPositions;
-    int fireCount;
+    Player player;
 
     public GamePanel(){
         background = new Background();
-        playerImage = SpriteUtils.loadImage("assets/images/players/straight/0.png");
-        playerPosition = new Vector2D(200,500);
-        bulletImage = SpriteUtils.loadImage("assets/images/player-bullets/a/1.png");
-        bulletPositions = new ArrayList<>();
-        bulletPositions.add(new Vector2D(200,400));
-        fireCount =0;
+        player = new Player();
         //bulletPositions.add() //them phan tu vao mang
 //        bulletPositions.get()// lay ra phan tu o vitri cu the
 //        bulletPositions.size()// lay ra kich thuoc cua mang
@@ -36,13 +26,8 @@ public class GamePanel extends JPanel {
 
         // draw image
         background.render(g);
+        player.render(g);
 
-        g.drawImage(playerImage, (int) playerPosition.x, (int) playerPosition.y, null);
-
-        for (int i =0 ; i<bulletPositions.size();i++){
-            Vector2D bulletPosition = bulletPositions.get(i);
-            g.drawImage(bulletImage,(int)bulletPosition.x,(int)bulletPosition.y,null);
-        }
     }
 
 
@@ -70,65 +55,11 @@ public class GamePanel extends JPanel {
 
     private void runAll() {
         background.run();
-        playerRun();
-        bulletsRun();
-
-    }
-
-    private void bulletsRun() {
-        for (int i = 0; i< bulletPositions.size(); i++){
-            Vector2D bulletPosition= bulletPositions.get(i);
-            bulletPosition.y -=3 ;
-        }
-    }
-
-    private void playerRun() {
-        playerMove();
-        playerlimit();
-        playerFire();
-
-    }
-
-    private void playerFire() {
-        fireCount++;
-        if (GameWindow.isFirePress && fireCount > 20){
-            Vector2D bulletPosition =  new Vector2D(playerPosition.x ,playerPosition.y);
-            bulletPositions.add(bulletPosition);
-            fireCount =0;
-        }
-    }
-
-    private void playerlimit() {
-        if (playerPosition.y < 0) {// limit player
-            playerPosition.y = 0;
-        }
-        if (playerPosition.y > 600-48) {
-            playerPosition.y = 600-48;
-        }
-        if (playerPosition.x < 0) {
-            playerPosition.x = 0;
-        }
-        if (playerPosition.x > 384-32) {
-            playerPosition.x = 384-32;
-        }
+        player.run();
 
 
     }
 
-    private void playerMove() {
-        if (GameWindow.isUpPress ) {
-            playerPosition.y--;
-        }
-        if (GameWindow.isDownPress ) {
-            playerPosition.y++;
-        }
-        if (GameWindow.isLeftPress ) {
-            playerPosition.x--;
-        }
-        if (GameWindow.isRightPress ) {
-            playerPosition.x++;
-        }
-    }
 
 
 
--- src/game/Vector2D.java
+++ src/game/Vector2D.java
@@ -22,7 +22,7 @@ public class Vector2D {
 
     public Vector2D clone(){//
 
-        return null;
+        return new Vector2D(this.x,this.y);
     }
 
     public void  set(double x, double y){
@@ -45,31 +45,65 @@ public class Vector2D {
 
     public void scale(double rate){
         // nhan gia tri rate vao vector dc goi
-        this.x *= x ;
-        this.y *= y ;
+        this.x *= rate ;
+        this.y *= rate ;
     }
 
     public double getLength(){
-        double z = Math.hypot(x,y) ;
-
-        return 0;// tra ra do dai cua vector dc goi
+        //double z = Math.hypot(x,y) ;
+        return Math.sqrt(this.x * this.x + this.y * this.y);// tra ra do dai cua vector dc goi
     }
 
     public double setLength(double length){
         //giu nguyen gia tri huong cua vector dc goi , thay doi do dai = do dai truyen vao
-
-
+        double currentLength = this.getLength();
+        if (currentLength != 0){
+            this.x = this.x * length / currentLength;
+            this.y = this.y * length / currentLength;
+        }
         return 0;
     }
 
     public double getAngle(){
-        double z = Math.hypot(x,y) ;
-        double t = x/z;
-        double c = Math.acos(t);
-        return 0;// tra ra goc tao giua vector vs truc hoanh(x,y) vs (1,0)
+//        double z = Math.hypot(x,y) ;
+//        double t = x/z;
+//        double c = Math.acos(t);
+//        double tanAngle = this.y / this.x;
+//        double angle =  Math.atan(tanAngle);
+//        return angle;
+
+
+        return Math.atan(this.y / this.x);// tra ra goc tao giua vector vs truc hoanh(x,y) vs (1,0)
     }
 
     public void setAngle(double angle){
         // giu nguyen do dai cua vector , quay vector den goc truyen vao
+        double length = this.getLength();
+        if (length != 0 ){
+            this.x = length * Math.cos(angle);
+            this.y = length * Math.sin(angle);
+        }
+    }
+    // this dai dien cho doi tuong dc tao ra tu classs
+
+    public static void main(String[] args) {
+        Vector2D v1 = new Vector2D(1,3);
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
